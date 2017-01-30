dea48d27ffe8dccaaa129b933e673f6d4af3d43a
Azizur Rahman <azizur.ice@gmail.com>
2017-01-30 01:55:33 -0800

    Login activity added

---
+++ app/src/main/java/com/ps/touchcounter/activities/LoginActivity.java
@@ -0,0 +1,357 @@
+package com.ps.touchcounter.activities;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.TwitterAuthProvider;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.ps.touchcounter.BaseActivity;
+import com.ps.touchcounter.R;
+import com.ps.touchcounter.model.User;
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
+
+import io.fabric.sdk.android.Fabric;
+
+public class LoginActivity extends BaseActivity implements  View.OnClickListener {
+    private static final String TAG = LoginActivity.class.getSimpleName();
+    private static final int RC_SIGN_IN = 9001;
+
+    // Authentication with firebase backend services
+    private FirebaseAuth mAuth;
+    private DatabaseReference mDatabase;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    //private CallbackManager mCallbackManager;
+
+    // Custom user login
+    private ImageView mAppLogo;
+    private EditText mEmailField;
+    private EditText mPasswordField;
+    private Button mSignInButton;
+    private Button mSignUpButton;
+    private SignInButton mGooglePlusButton;
+    //private Button mTwitterButton;
+    TwitterLoginButton mTwitterButton;
+    private String socialFlag = "Twitter";
+
+    ViewGroup mMainContainer;
+    LinearLayout signInContainer, signUpContainer;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        mMainContainer = (ViewGroup)findViewById(R.id.main_container);
+        signInContainer = (LinearLayout) findViewById(R.id.signIn_container);
+        signUpContainer = (LinearLayout) findViewById(R.id.signUp_container);
+
+        LayoutInflater layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        //Inject the views (Custom Login for SingIn and SingUp) in the respective containers
+        signInContainer.addView(layoutInflater.inflate(R.layout.fragment_custom_login, mMainContainer, false));
+        signInContainer.addView(layoutInflater.inflate(R.layout.fragment_divider_login, mMainContainer, false));
+        signUpContainer.addView(layoutInflater.inflate(R.layout.fragment_sign_up, mMainContainer, false));
+        //Hide necessary views
+        signUpContainer.setVisibility(View.GONE);
+
+
+        // Twitter Auth setting
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(
+                getString(R.string.twitter_consumer_key),
+                getString(R.string.twitter_consumer_secret));
+        Fabric.with(this, new Twitter(authConfig));
+
+
+      // Twitter Button
+        signInContainer.addView(layoutInflater.inflate(R.layout.fragment_twitter_login, mMainContainer, false));
+        mTwitterButton = (TwitterLoginButton) findViewById(R.id.twitter_login_button);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mAuth = FirebaseAuth.getInstance();
+
+        // Views
+        mEmailField = (EditText) findViewById(R.id.field_email);
+        mPasswordField = (EditText) findViewById(R.id.field_password);
+        mSignInButton = (Button) findViewById(R.id.button_sign_in);
+        mSignUpButton = (Button) findViewById(R.id.button_sign_up);
+
+
+        // [START auth_state_listener for Twitter]
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                updateUI(user);
+            }
+        };
+        // [END auth_state_listener]
+
+
+
+        // Click listeners
+        mSignInButton.setOnClickListener(this);
+        mSignUpButton.setOnClickListener(this);
+        mTwitterButton.setOnClickListener(this);
+
+        mTwitterButton.setCallback(new Callback<TwitterSession>() {
+            @Override
+            public void success(Result<TwitterSession> result) {
+                Log.d(TAG, "twitterLogin:success" + result);
+                handleTwitterSession(result.data);
+            }
+
+            @Override
+            public void failure(TwitterException exception) {
+                Log.w(TAG, "twitterLogin:failure", exception);
+                updateUI(null);
+            }
+        });
+        // [END initialize_twitter_login]
+   }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+//         Check auth on Activity start
+        if (mAuth.getCurrentUser() != null) {
+            onAuthSuccess(mAuth.getCurrentUser());
+        }
+    }
+
+    ////////////////////////
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+    // [END on_stop_remove_listener]
+
+
+
+
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d(TAG," Result has come back");
+        if (socialFlag.equalsIgnoreCase("Twitter")){
+            Log.d(TAG," Result has come back :"+ socialFlag);
+            mTwitterButton.onActivityResult(requestCode, resultCode, data);  // Twitter
+        }
+    }
+
+    // [START auth_with_twitter]
+    private void handleTwitterSession(TwitterSession session) {
+        Log.d(TAG, "handleTwitterSession:" + session);
+        showProgressDialog();
+
+        AuthCredential credential = TwitterAuthProvider.getCredential(
+                session.getAuthToken().token,
+                session.getAuthToken().secret);
+
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithCredential:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithCredential", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                        //onAuthSuccess(task.getResult().getUser());
+                        updateUI(task.getResult().getUser());
+
+                        // [START_EXCLUDE]
+                        hideProgressDialog();
+                        // [END_EXCLUDE]
+                    }
+                });
+    }
+    // [END auth_with_twitter]
+
+
+    private void signOutTwitter() {
+        mAuth.signOut();
+        Twitter.logOut();
+        updateUI(null);
+    }
+
+
+
+    private void updateUI(FirebaseUser user) {
+        hideProgressDialog();
+        if (user != null) {
+            Log.d(TAG, " User Name : " + user.getDisplayName());
+            Log.d(TAG, " User Id : " + user.getUid());
+            Intent intent = new Intent(this, TouchCountActivity.class);
+            startActivity(intent);
+        } else {
+            Log.d(TAG, " Not successfully signed in with Twitter authentication.");
+
+        }
+    }
+
+    ////////////////////////////////////////////
+
+    private void signIn() {
+        Log.d(TAG, "signIn..................................................");
+        if (!validateForm()) {
+            return;
+        }
+
+        showProgressDialog();
+        String email = mEmailField.getText().toString();
+        String password = mPasswordField.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signIn:onComplete:" + task.isSuccessful());
+                        hideProgressDialog();
+
+                        if (task.isSuccessful()) {
+                            onAuthSuccess(task.getResult().getUser());
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Sign In Failed",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void signUp() {
+        Log.d(TAG, "signUp");
+        if (!validateForm()) {
+            return;
+        }
+
+        showProgressDialog();
+        String email = mEmailField.getText().toString();
+        String password = mPasswordField.getText().toString();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "createUser:onComplete:" + task.isSuccessful());
+                        hideProgressDialog();
+
+                        if (task.isSuccessful()) {
+                            onAuthSuccess(task.getResult().getUser());
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Sign Up Failed",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void onAuthSuccess(FirebaseUser user) {
+        String username = usernameFromEmail(user.getEmail());
+
+        // Write new user
+        writeNewUser(user.getUid(), username, user.getEmail());
+
+        // Go to MainActivity
+        startActivity(new Intent(LoginActivity.this, TouchCountActivity.class));
+        finish();
+    }
+
+    private String usernameFromEmail(String email) {
+        if (email.contains("@")) {
+            return email.split("@")[0];
+        } else {
+            return email;
+        }
+    }
+
+    private boolean validateForm() {
+        boolean result = true;
+        if (TextUtils.isEmpty(mEmailField.getText().toString())) {
+            mEmailField.setError("Required");
+            result = false;
+        } else {
+            mEmailField.setError(null);
+        }
+
+        if (TextUtils.isEmpty(mPasswordField.getText().toString())) {
+            mPasswordField.setError("Required");
+            result = false;
+        } else {
+            mPasswordField.setError(null);
+        }
+
+        return result;
+    }
+
+    // [START basic_write]
+    private void writeNewUser(String userId, String name, String email) {
+        User user = new User(name, email);
+        mDatabase.child("users").child(userId).setValue(user);
+    }
+    // [END basic_write]
+
+    @Override
+    public void onClick(View v) {
+        int i = v.getId();
+        if (i == R.id.button_sign_in) {
+            signIn();
+        } else if (i == R.id.button_sign_up) {
+            signUp();
+        }else if (i == R.id.twitter_login_button){
+            socialFlag = "Twitter";
+            Log.d(TAG,"Precess Twitter Login button");
+        }
+
+
+    }
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        hideProgressDialog();
+    }
+
+}
