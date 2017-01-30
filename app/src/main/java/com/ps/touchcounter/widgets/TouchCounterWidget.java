0ab02f23dc52dfe6dcd7f808633d515174e67b14
Azizur Rahman <azizur.ice@gmail.com>
2017-01-30 03:13:39 -0800

    Changed service to IntentService

--- app/src/main/java/com/ps/touchcounter/widgets/TouchCounterWidget.java
+++ app/src/main/java/com/ps/touchcounter/widgets/TouchCounterWidget.java
@@ -18,7 +18,7 @@ import android.widget.RemoteViews;
 
 import com.ps.touchcounter.R;
 import com.ps.touchcounter.activities.LoginActivity;
-import com.ps.touchcounter.services.UpdateActivityService;
+//import com.ps.touchcounter.services.UpdateActivityService;
 
 import java.util.Calendar;
 
@@ -27,15 +27,13 @@ import java.util.Calendar;
  */
 public class TouchCounterWidget extends AppWidgetProvider {
 
-    public static final int REQUEST_CODE = 12345;
-    public static final String ACTION = "com.ps.touchcounter.servicesdemo.alarm";
-
-    // Triggered by the Alarm periodically (starts the service to run task)
-    @Override
-    public void onReceive(Context context, Intent intent) {
-        Intent i = new Intent(context, AppWidgetService.class);
-        context.startService(i);
-    }
+    //public static final int REQUEST_CODE = 12345;
+    // Triggered by the Alarm periodically but we are not using AlarmManager, so comment it
+//    @Override
+//    public void onReceive(Context context, Intent intent) {
+//        Intent i = new Intent(context, AppWidgetService.class);
+//        context.startService(i);
+//    }
 
 //    @Override
 //    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
@@ -48,12 +46,8 @@ public class TouchCounterWidget extends AppWidgetProvider {
 //    @Override
 //    public void onEnabled(Context context) {
 //        super.onEnabled(context);
-//
-////        context.startService(new Intent(UpdateTimeService.UPDATE_TIME));
-//        //  context.startService(new Intent(UpdateTimeService.UPDATE_TIME).setPackage("com.ps.digitalclock"));
-//        context.startService(new Intent(AppWidgetService.UPDATE_TOUCH_RATE).setPackage("com.ps.touchcounter"));
+ //        context.startService(new Intent(AppWidgetService.UPDATE_TOUCH_RATE).setPackage("com.ps.touchcounter"));
 //        //context.startService(new Intent(context,AppWidgetService.class));
-//
 //    }
 //
 //    @Override
@@ -64,17 +58,17 @@ public class TouchCounterWidget extends AppWidgetProvider {
 //        context.stopService(new Intent(context,AppWidgetService.class));
 //    }
 
-    public static  final class AppWidgetService extends Service {
+    public static  final class AppWidgetService extends IntentService {
         private static final String TAG = AppWidgetService.class.getSimpleName();
        // AlarmManager alarmManager;
-        static final String UPDATE_TOUCH_RATE = "com.ps.touchcounter.widgets.action.UPDATE_TOUCH_RATE";
+        public static final String UPDATE_TOUCH_RATE = "com.ps.touchcounter.widgets.action.UPDATE_TOUCH_RATE";
         private final static IntentFilter mIntentFilter = new IntentFilter();
         static int counter=1;
         private Calendar mCalendar;
 
-//        public AppWidgetService(){
-//            super("UpdateActivityService");
-//        }
+        public AppWidgetService(){
+            super("UpdateActivityService");
+        }
 
 //        static {
 //            // mIntentFilter.addAction(UPDATE_TOUCH_RATE);
@@ -100,86 +94,24 @@ public class TouchCounterWidget extends AppWidgetProvider {
         }
 
         @Override
-        public int onStartCommand(Intent intent, int flags, int startId) {
-            super.onStartCommand(intent, flags, startId);
-
-            if (intent != null) {
-              //  if (UPDATE_TOUCH_RATE.equals(intent.getAction())) {
-                    //updateTime();
-                    useHandlerForFrequentCall();
-                //}
-            }
-
-            return START_STICKY;
+        protected void onHandleIntent(Intent intent) {
+            updateTouchRate(intent.getStringExtra(UPDATE_TOUCH_RATE));
         }
 
-        @Override
-        public IBinder onBind(Intent intent) {
-            return null;
-        }
-
-
-//        private final BroadcastReceiver mTouchRateChangedReceiver = new BroadcastReceiver() {
-//            @Override
-//            public void onReceive(Context context, Intent intent) {
-//                updateTime();
-//            }
-//        };
-
-
-
-        private void updateTime() {
-            Log.d(TAG, "Running update  "+ "time");
+        private void updateTouchRate(String touchRate) {
             mCalendar.setTimeInMillis(System.currentTimeMillis());
             counter = counter + 1;
+            if (counter>500000) counter=0;
+
             RemoteViews mRemoteViews = new RemoteViews(getPackageName(), R.layout.touch_counter_widget);
             mRemoteViews.setTextViewText(R.id.timeId,
                     DateFormat.format(getString(R.string.time_format), mCalendar));
-            if (isMyServiceRunning(UpdateActivityService.class)) {
-                mRemoteViews.setTextViewText(R.id.appwidget_text, UpdateActivityService.resultTxt + " " + counter);
-            } else {
-                mRemoteViews.setTextViewText(R.id.appwidget_text, "Test " + ": " + counter);
-            }
+            mRemoteViews.setTextViewText(R.id.appwidget_text, touchRate + " Test Counter : " + counter);
+
             ComponentName mComponentName = new ComponentName(this, TouchCounterWidget.class);
             AppWidgetManager mAppWidgetManager = AppWidgetManager.getInstance(this);
             mAppWidgetManager.updateAppWidget(mComponentName, mRemoteViews);
         }
-
-        private boolean isMyServiceRunning(Class<?> serviceClass) {
-            ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
-            for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
-                if (serviceClass.getName().equals(service.service.getClassName())) {
-                    return true;
-                }
-            }
-            return false;
-        }
-
-//
-//        @Override
-//        protected void onHandleIntent(Intent intent) {
-//            //        Intent i = new Intent(context, AppWidgetService.class);
-////        context.startService(i);
-//            Log.d(TAG, "Service Running "+intent.getAction());
-//            useHandlerForFrequentCall();
-//        }
-
-        private void useHandlerForFrequentCall(){
-            Handler handler = new Handler();
-            for(int i=0; i<600; i++) {
-                handler.postDelayed(runnableCode, 100);
-
-            }
-        }
-
-        private Runnable runnableCode = new Runnable() {
-            @Override
-            public void run() {
-                updateTime();
-
-            }
-        };
-
     }
 }
 
